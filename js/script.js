f0533273167bcd797134a653af210e7d28ba0526
Marko Kojičić <marko.kojicic@gmail.com>
2025-05-12 09:59:38 +0200

    Changed font to montserrat

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function scrollGallery(direction) {
     const container = document.querySelector('.gallery .content .images');
-    const scrollAmount = container.offsetWidth; // Scroll half the container width
+    const scrollAmount = container.offsetWidth; //Scroll full container width (slide)
     container.scrollBy({
         left: direction * scrollAmount,
         behavior: 'smooth'
